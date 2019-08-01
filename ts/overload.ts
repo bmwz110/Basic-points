c32fce05eda7db3ace9599d7666db7898809d63c
bmwz110 <bmwz110@mail.dlut.edu.cn>
2019-08-01 19:32:14 +0800

    overload

---
+++ ts/overload.ts
@@ -0,0 +1,22 @@
+function padding(all: number);
+function padding(topAndBottom: number, leftAndRight: number);
+function padding(top: number, right: number, bottom: number, left: number);
+// Actual implementation that is a true representation of all the cases the function body needs to handle
+function padding(a: number, b?: number, c?: number, d?: number) {
+  if (b === undefined && c === undefined && d === undefined) {
+    b = c = d = a;
+  } else if (c === undefined && d === undefined) {
+    c = a;
+    d = b;
+  }
+  return {
+    top: a,
+    right: b,
+    bottom: c,
+    left: d
+  };
+}
+console.log(padding(1))
+console.log(padding(1,2))
+console.log(padding(1,2,3))   // error
+console.log(padding(1,2,3,4))
