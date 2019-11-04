9b3eed2f2e8f7b3bb74bd777df1e1ed7227471f9
bmwz110 <bmwz110@mail.dlut.edu.cn>
2019-11-04 13:33:01 +0800

    Update global_var&attr.js

--- js/global_var&attr.js
+++ js/global_var&attr.js
@@ -1,5 +1,8 @@
 // global.a 或 b 是在全局注册了一个property并赋值
-// var/let c 是在全局创建了一个变量并赋值
+// var/let c 是在全局创建了一个variable并赋值
+
+// 注意： let a = b = 2 会被解析为 let a = (b = 2)，此时认为创建了一个global variable：b
+
 global.a = 1;
 b = 2;
 let c = 3;
@@ -12,5 +15,3 @@ function foo() {
 }
 
 foo();
-
-// 注意： let a = b = 2 会被解析为 let a = (b = 2)，此时认为创建了一个global variable：b
