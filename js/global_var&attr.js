501653224c90c92f469f9fead0080ed9652385df
bmwz110 <bmwz110@mail.dlut.edu.cn>
2019-11-04 12:01:41 +0800

    Update global_var&attr.js

--- js/global_var&attr.js
+++ js/global_var&attr.js
@@ -1,10 +1,11 @@
-// global.a 或 b 是在全局注册了一个属性并赋值
+// global.a 或 b 是在全局注册了一个property并赋值
 // var/let c 是在全局创建了一个变量并赋值
 global.a = 1;
 b = 2;
-var c = 3;
+let c = 3;
 
 function foo() {
+  // foo()中的this指向foo()所在的全局，而非foo()自己
   console.log(this.a);   // 1
   console.log(this.b);   // 2
   console.log(this.c);   // undefined
